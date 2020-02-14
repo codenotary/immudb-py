8be7e88256cde615389a0f8db976bcd5b3696201
Leonardo Grasso <me@leonardograsso.com>
2020-02-14 12:02:13 +0100

    feat(immu): inclusion proof verifier

---
+++ immu/inclusion.py
@@ -0,0 +1,38 @@
+import hashlib
+
+from immu.schema import schema_pb2
+from immu import constants
+
+def path_verify(path:list, at:int, i:int, root:bytes, leaf:bytes) -> bool:
+    if i > at or (at > 0 and len(path) == 0):
+        return False
+    h = leaf
+    for v in path:
+        c = bytearray()
+        c.append(constants.NODE_PREFIX)
+        v = bytes(v)
+        if i%2 == 0 and i != at:
+            c.extend(h)
+            c.extend(v)
+        else:
+            c.extend(v)
+            c.extend(h)
+        h = hashlib.sha256(c).digest()
+        i = i // 2
+        at = at // 2
+    return at == i and h == root
+        
+        
+
+
+
+def verify(inclusionProof: schema_pb2.InclusionProof, index: int, leaf: bytes) -> bool:
+    if inclusionProof.index != index or bytes(inclusionProof.leaf) != leaf:
+        return False
+    return path_verify(
+        path=inclusionProof.path, 
+        at=inclusionProof.at, 
+        index=inclusionProof.index, 
+        root=bytes(inclusionProof.root), 
+        leaf=leaf
+        )
---
+++ tests/immu/test_inclusion.py
@@ -0,0 +1,12 @@
+import pytest
+
+from immu import inclusion
+from immu.schema import schema_pb2
+
+class TestInclusion:
+    def test_verify_path(self):
+        path = []
+        assert True == inclusion.path_verify(path, 0, 0, bytes(), bytes())
+        assert False == inclusion.path_verify(path, 0, 1, bytes(), bytes())
+        assert False == inclusion.path_verify(path, 1, 0, bytes(), bytes())
+        assert False == inclusion.path_verify(path, 1, 1, bytes(), bytes())
