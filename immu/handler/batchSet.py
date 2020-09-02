29b516388c68b00a44a66b6de80099fa8183e199
Simone Lazzaris <slazzaris@gmail.com>
2020-09-02 15:04:54 +0200

    batchGet and batchSet progress

---
+++ immu/handler/batchSet.py
@@ -0,0 +1,35 @@
+import struct
+from time import time
+from dataclasses import dataclass
+
+from immu.schema import schema_pb2
+from immu.service import schema_pb2_grpc
+from immu.rootService import RootService
+from immu import constants, proofs, item
+
+@dataclass
+class batchSetResponse:
+    index: schema_pb2.Index
+
+def _packValueTime(kv,tstamp):
+    valueBytes = bytearray()
+    valueBytes.extend(struct.pack('>Q', tstamp))
+    valueBytes.extend(kv.value)
+    kv.value=bytes(valueBytes)
+    return kv
+    
+def call(service: schema_pb2_grpc.ImmuServiceStub, rs: RootService, request: schema_pb2.KVList):
+    root = rs.get()
+    index = schema_pb2.Index(index = root.index)
+
+    currtime=int(time()*1000)
+    
+    rawRequest = schema_pb2.KVList(
+        KVs = [_packValueTime(kv,currtime) for kv in request.KVs],
+        #rootIndex = index
+    )
+
+    idx = service.SetBatch(rawRequest)
+    return batchSetResponse(
+        index = idx
+    )
---
+++ tests/immu/test_get_set.py
@@ -0,0 +1,34 @@
+import pytest
+from immu.client import ImmuClient
+from random import randint
+
+class TestGetSet:
+        
+    def test_get_set(self):
+        a = ImmuClient("localhost:3322")
+        a.login("immudb","immudb")
+        key="test_key_{:04d}".format(randint(0,10000))
+        value="test_value_{:04d}".format(randint(0,10000))
+
+        a.safeSet(key.encode('utf8'),value.encode('utf8'))
+        readback=a.safeGet(key.encode('utf8')).value
+
+        assert value==readback
+
+    def test_get_set_batch(self):
+        a = ImmuClient("localhost:3322")
+        a.login("immudb","immudb")
+        xset=[
+            {'key':b'gorilla', 'value':b'banana'},
+            {'key':b'zebra',   'value':b'grass'},
+            {'key':b'lion',    'value':b'zebra'}
+            ]
+        assert type(a.setAll(xset))!=int
+        xget=[x['key'] for x in xset]
+        res=a.getAll(xget)
+        for i in res.itemlist.items:
+            for j in filter(lambda z:z['key']==i.key, xset):
+                assert j['value']==i.value
+
+
+
