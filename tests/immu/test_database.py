93dbd129dc0515fe9e78f8afc1fe705a07429e79
Simone Lazzaris <slazzaris@gmail.com>
2020-09-03 12:48:17 +0200

    test added

---
+++ tests/immu/test_database.py
@@ -0,0 +1,18 @@
+import pytest
+from immu.client import ImmuClient
+from random import randint
+import grpc._channel
+
+class TestDatabase:
+        
+    def test_list_use(self):
+        try:
+            a = ImmuClient("localhost:3322")
+            a.login("immudb","immudb")
+        except grpc._channel._InactiveRpcError as e:
+            pytest.skip("Cannot reach immudb server")
+        resp=a.databaseList()
+        assert resp.dblist.databases[0].databasename=="defaultdb"
+        resp=a.databaseUse("defaultdb")
+        assert type(resp.reply.token)==str
+
