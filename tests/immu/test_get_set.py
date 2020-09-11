822973935a64bac51f5c554ce102d51abcb59db1
Simone Lazzaris <slazzaris@gmail.com>
2020-09-11 16:23:35 +0200

    moved batch operations in distinct branch

--- tests/immu/test_get_set.py
+++ tests/immu/test_get_set.py
@@ -20,31 +20,5 @@ class TestGetSet:
         assert readback.verified==True
         assert value==readback.value
 
-    def test_get_set_batch(self):
-        try:
-            a = ImmudbClient("localhost:3322")
-            a.login("immudb","immudb")
-        except grpc._channel._InactiveRpcError as e:
-            pytest.skip("Cannot reach immudb server")
-        xset={
-            b'gorilla': b'banana',
-            b'zebra':   b'grass',
-            b'lion':    b'zebra'
-            }
-        assert type(a.setAll(xset))!=int
-        # test getAll
-        resp=a.getAll(xset.keys())
-        for i in resp.keys():
-            assert i in xset
-            assert xset[i]==resp[i]
-        for i in xset.keys():
-            assert i in resp
-            assert xset[i]==resp[i]
-        # test getAllItems
-        resp=a.getAllItems(xset.keys())
-        for i in resp.itemlist.items:
-            assert i.key in xset
-            assert xset[i.key]==i.value.payload
-
 
 
