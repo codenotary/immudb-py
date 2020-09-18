d77425e23cb3609fce8f64214acad5a84d535430
mabmayer <mabmayer@opvizor.com>
2020-09-18 12:23:38 +0200

    added functions ChangePassword, CreateUser, ListUsers (#10)

---
+++ immudb/handler/createUser.py
@@ -0,0 +1,17 @@
+from dataclasses import dataclass
+
+from immudb.schema import schema_pb2
+from immudb.service import schema_pb2_grpc
+from immudb.rootService import RootService
+from google.protobuf.empty_pb2 import Empty
+
+@dataclass
+class createUserResponse:
+    reply: Empty
+
+def call(service: schema_pb2_grpc.ImmuServiceStub, rs: RootService, request: schema_pb2.CreateUserRequest): 
+    
+    msg = service.CreateUser(request)
+    return createUserResponse(
+       reply = msg
+    )
