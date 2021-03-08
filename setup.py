7f624eadafee22c77ab176a51bfdb0dcceda9bd4
Simone Lazzaris <slazzaris@gmail.com>
2021-03-08 12:00:14 +0100

    release 0.9.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
     
 setup(name='immudb-py',
-      version='0.9.1rc1',
+      version='0.9.1',
       license="Apache License Version 2.0",
       description='Python SDK for Immudb',
       long_description=long_description,
