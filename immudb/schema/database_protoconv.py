36e1ef268d5125efa62cb84b14a8c0c7ed44be06
Michael Abmayer <mabmayer@opvizor.com>
2022-03-28 13:10:28 +0200

    fix: broken import

--- immudb/schema/database_protoconv.py
+++ immudb/schema/database_protoconv.py
@@ -11,7 +11,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from tomlkit import datetime
 from immudb.constants import *
 from immudb.embedded import store, htree
 import copy
