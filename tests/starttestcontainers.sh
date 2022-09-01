48a8ebee005a288156d7b7234ec7f5667f382752
Adam Raźniewski <adam@razniewski.eu>
2022-09-01 14:20:18 +0200

    Immudb 1.3.2 with backward compability fix (#53)
    * Backward compability fix for error detail

--- tests/starttestcontainers.sh
+++ tests/starttestcontainers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-docker run -d --health-cmd "immuadmin status" --health-interval 10s --health-timeout 5s --health-retries 5 -v "$PWD"/certs/my.key.pem:/key.pem -p 3322:3322 codenotary/immudb:1.3.1 --signingKey=/key.pem
+docker run -d --health-cmd "immuadmin status" --health-interval 10s --health-timeout 5s --health-retries 5 -v "$PWD"/certs/my.key.pem:/key.pem -p 3322:3322 codenotary/immudb:1.3.2 --signingKey=/key.pem
 docker run -d --health-cmd "immuadmin status" --health-interval 10s --health-timeout 5s --health-retries 5 -v "$PWD"/certs/my.key.pem:/key.pem -p 3333:3322 codenotary/immudb:1.2.4 --signingKey=/key.pem
 docker run -d --health-cmd "immuadmin status" --health-interval 10s --health-timeout 5s --health-retries 5 -v "$PWD"/certs/my.key.pem:/key.pem -p 3344:3322 codenotary/immudb:1.1.0 --signingKey=/key.pem
